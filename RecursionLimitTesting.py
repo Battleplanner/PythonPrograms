094029a3f4552ed2578471263be3b49e1d56f2ab
Battleplanner <mirak.ictman@gmail.com>
2019-03-19 21:23:31 +1300

    Creating the functions

--- RecursionLimitTesting.py
+++ RecursionLimitTesting.py
@@ -18,18 +18,18 @@ import time
 # recurse(1)
 #Without the failsafe, the program continued to run, stopping at 6783 before hitting the recursion limit
 
-# def recurse(n):
-#     print(n)
-#     sys.setrecursionlimit(n + 100)
-#     recurse(n + 1)
-#
-# recurse(1)
-# This managed to get slightly further by making sure the recursion limit was 100 more than the number, stopping at 6787
-
 def recurse(n):
     print(n)
-    sys.setrecursionlimit(n + 70)
-    time.sleep(0.01)
+    sys.setrecursionlimit(n + 100)
     recurse(n + 1)
 
 recurse(1)
+#This managed to get slightly further by making sure the recursion limit was 100 more than the number, stopping at 6787
+
+# def recurse(n):
+#     print(n)
+#     sys.setrecursionlimit(n + 70)
+#     time.sleep(0.01)
+#     recurse(n + 1)
+#
+# recurse(1)
